3b5f6e3542b55ccf3b246ad9bd4a851bb532a20b
omraj <omraj.da2@iiitmk.ac.in>
2025-08-19 16:12:46 +0530

    upd'

--- src/main/java/demo/com/example/omspring/config/SpringSecurityConfig.java
+++ src/main/java/demo/com/example/omspring/config/SpringSecurityConfig.java
@@ -5,21 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
-	/*
-	 * @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
-	 * Exception { http.csrf().disable().authorizeHttpRequests(auth -> auth
-	 * .requestMatchers("/users/register", "/users/login").permitAll()
-	 * //.requestMatchers("/users/register", "/users/login").permitAll()
-	 * .anyRequest().authenticated()).oauth2Login(withDefaults()); return
-	 * http.build(); }
-	 */
-	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	    http.csrf().disable()
--- src/main/java/demo/com/example/omspring/controller/UserController.java
+++ src/main/java/demo/com/example/omspring/controller/UserController.java
@@ -1,17 +1,46 @@
 package demo.com.example.omspring.controller;
 
+import java.util.Map;
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.AccessTokenResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.sun.istack.NotNull;
+
+import demo.com.example.omspring.model.AuthRequest;
 import demo.com.example.omspring.model.UserModel;
 import demo.com.example.omspring.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+	@Value("${keycloak.auth-server-url}")
+	private String keycloakBaseUrl;
+
+	@Value("${keycloak.realm}")
+	private String realm;
+
+	@Value("${keycloak.resource}")
+	private String clientId;
+
+	@Value("${keycloak.credentials.secret}")
+	private String clientSecret;
+
 	private final UserService userService;
 
 	public UserController(UserService userService) {
@@ -33,4 +62,29 @@ public class UserController {
 	public String secure() {
 		return "You are logged in!";
 	}
+
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody Map<String, String> loginRequest) {
+
+		String username = loginRequest.get("username");// loginRequest.getUsername();
+		String password = loginRequest.get("password");// loginRequest.getPassword();
+
+		String tokenUrl = keycloakBaseUrl + "/realms/" + realm + "/protocol/openid-connect/token";
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		String body = "client_id=" + clientId + "&client_secret=" + clientSecret + "&username=" + username
+				+ "&password=" + password + "&grant_type=password";
+
+		HttpEntity<String> entity = new HttpEntity<>(body, headers);
+
+		try {
+			ResponseEntity<Map> response = restTemplate.exchange(tokenUrl, HttpMethod.POST, entity, Map.class);
+			return ResponseEntity.ok(response.getBody());
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials!");
+		}
+	}
+
 }
